14a98f0e52c8df8a3d2be2d180125e1b55eac0e4
travisprol <travisjohnprol@gmail.com>
2020-06-06 09:25:45 -0400

    Done.

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,30 @@
 class Customer
   
+    @@all = []
+    
+    def initialize(name, age)
+        @name = name
+        @age = age
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def new_meal(waiter, total, tip=0)
+        Meal.new(waiter, self, total, tip)
+    end
+
+    def meals
+        Meal.all.select do |meal|
+            meal.customer == self
+        end
+    end
+
+    def waiters
+        Waiter.all.select do |meal|
+            meal.customer == self
+        end
+    end
 end
--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,14 @@
 class Waiter
 
+    @@all = []
+
+    def initialize(name, yrs_experience)
+        @name = name
+        @yrs_experience = yrs_experience
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
 end
